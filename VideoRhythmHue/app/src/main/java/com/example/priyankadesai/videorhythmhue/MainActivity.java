8b5855c1b969ebd0aab58c7f83ac3537442172ba
Rajvi Mistry <rmistry2@binghamton.edu>
2017-11-11 00:52:50 -0500

    Navigate to another app from Main Activity on button press

--- VideoRhythmHue/app/src/main/java/com/example/priyankadesai/videorhythmhue/MainActivity.java
+++ VideoRhythmHue/app/src/main/java/com/example/priyankadesai/videorhythmhue/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.button1).setOnClickListener(this);
         findViewById(R.id.button2).setOnClickListener(this);
     }
 
@@ -22,11 +23,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void openPhilipsHueApp(View view) {
         // TODO
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.philips.lighting.hue2");
+        if (launchIntent != null) {
+            startActivity(launchIntent);//null pointer check in case package name was not found
+        }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.button1:
+                openPhilipsHueApp(v);
+                break;
+
             case R.id.button2:
                 openVideoPlayer(v);
                 break;
